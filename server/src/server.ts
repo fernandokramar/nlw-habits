2c28e35095de8ca2121b93e03db8874d30352c41
Fernando Macedo Kramar <macedokramar@gmail.com>
2023-01-24 11:40:59 -0300

    bug solved

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,11 @@ import { appRoutes } from "./routes";
 
 const app = Fastify();
 
-app.register(Cors, {
-  origin: ["http://localhost:3000"],
-});
+// app.register(Cors, {
+//   origin: ["http://localhost:3000", "http://localhost:5173", "exp://192.168.100.128:19000"],
+// });
+
+app.register(Cors)
 
 app.register(appRoutes);
 
